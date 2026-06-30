202a824d70e35b13c5d12664727f59b45625c9ef
Vishwansh Kumar <kumarvishwansh@gmail.com>
2026-06-30 16:27:50 +0530

    env

--- src/main/java/com/sduiBackend/api/ApiApplication.java
+++ src/main/java/com/sduiBackend/api/ApiApplication.java
@@ -1,9 +1,11 @@
 package com.sduiBackend.api;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableAsync;
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 //@EnableJpaRepositories("com.sduiBackend.repository")
@@ -11,6 +13,11 @@ import org.springframework.scheduling.annotation.EnableAsync;
 public class ApiApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+		System.out.println("Loaded DB URL: " + dotenv.get("NEON_DATABASE_URL"));
+		System.setProperty("NEON_DATABASE_URL", dotenv.get("NEON_DATABASE_URL"));
+		System.setProperty("DBUSER", dotenv.get("DBUSER"));
+		System.setProperty("PASSWORD", dotenv.get("PASSWORD"));
 		SpringApplication.run(ApiApplication.class, args);
 	}
 
